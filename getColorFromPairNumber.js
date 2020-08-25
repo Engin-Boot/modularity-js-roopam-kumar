16b3d06eca9a16d2ea7e85a0ac8d69e327f1ce99
roopam-kumar <46059596+roopam-kumar@users.noreply.github.com>
2020-08-25 11:46:02 +0530

    update file

--- getColorFromPairNumber.js
+++ getColorFromPairNumber.js
@@ -1,1 +1,27 @@
+const expect = require('chai').expect;
 
+const MajorColors = [
+    "white",
+    "red",
+    "black",
+    "yellow",
+    "violet"
+];
+
+const MinorColors = [
+    "blue",
+    "orange",
+    "green",
+    "brown",
+    "slate"
+];
+
+function GetColorFromPairNumber(pairNumber) {
+    let colorPair = {};
+    const zeroBasedPairNumber = pairNumber - 1;
+    const majorColorIndex =
+        Math.floor(zeroBasedPairNumber / MinorColors.length);
+    colorPair.major = MajorColors[majorColorIndex];
+    colorPair.minor = MinorColors[zeroBasedPairNumber % MinorColors.length];
+    return colorPair
+}
