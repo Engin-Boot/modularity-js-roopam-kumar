dd44523a6ab023981cc89150e8cd29bc25f120d2
roopam-kumar <46059596+roopam-kumar@users.noreply.github.com>
2020-08-25 12:41:49 +0530

    update with export

--- getPairNumberFromColors.js
+++ getPairNumberFromColors.js
@@ -33,4 +33,4 @@ function GetPairNumberFromColors(majorColor, minorColor) {
     finalNumber = majorIndex * MinorColors.length + minorIndex + 1;
     return finalNumber;
 }
-export{GetPairNumberFromColors};
+export { GetPairNumberFromColors };
