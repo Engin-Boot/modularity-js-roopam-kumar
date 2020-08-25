d1d8e1e0f729cd9f80e5f312c23e93069e8c5a6f
roopam-kumar <46059596+roopam-kumar@users.noreply.github.com>
2020-08-25 12:17:15 +0530

    update with import

--- testColorToNumber.js
+++ testColorToNumber.js
@@ -16,18 +16,10 @@ const MinorColors = [
     "slate"
 ];
 
-function include(file) { 
-  
-  var script  = document.createElement('script'); 
-  script.src  = file; 
-  script.type = 'text/javascript'; 
-  script.defer = true; 
-  
-  document.getElementsByTagName('head').item(0).appendChild(script); 
-  
-} 
 
-include(getPairNumberFromColors.js);
+import{getPairNumberFromColors} from './getPairNumberFromColors.js';
+
+
 
 function testColorToNumber(majorColor, minorColor, expectedNumber) {
     const pairNumber = GetPairNumberFromColors(majorColor, minorColor);
