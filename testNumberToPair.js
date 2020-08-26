dd2637992e36979dff97420eee5d18f5b50a6399
roopam-kumar <46059596+roopam-kumar@users.noreply.github.com>
2020-08-26 12:55:25 +0530

    update

--- testNumberToPair.js
+++ testNumberToPair.js
@@ -1,5 +1,4 @@
 const expect = require('chai').expect;
-//const GetColorFromPairNumber = require('./getColorFromPairNumber.js');
 const obj2 = require('./getColorFromPairNumber');
 
 const MajorColors = [
@@ -18,15 +17,10 @@ const MinorColors = [
     "slate"
 ];
 
-//import {GetColorFromPairNumber} from './getColorFromPairNumber';
-
 function testNumberToPair(number, expectedMajor, expectedMinor) {
     const pairOfColors = obj2.GetColorFromPairNumber(number);
     console.log(`${number} = ${expectedMajor} ${expectedMinor}`);
     expect(pairOfColors.major).equals(expectedMajor);
     expect(pairOfColors.minor).equals(expectedMinor);
 }
-//export {testNumberToPair};
 exports.testNumberToPair = testNumberToPair;
-//module.exports = { testNumberToPair,
-                 //};
